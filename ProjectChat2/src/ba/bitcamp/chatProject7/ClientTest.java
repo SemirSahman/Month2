d58a1368bb1ad1dff863e96611f1c95c1c4ea8b6
Semir Sahman <semir.sahman@BitCamps-Mac-mini-49.local>
2015-08-13 17:12:24 +0200

    Project Chat

---
+++ ProjectChat2/src/ba/bitcamp/chatProject7/ClientTest.java
@@ -0,0 +1,15 @@
+package ba.bitcamp.chatProject7;
+
+import javax.swing.JFrame;
+
+public class ClientTest {
+
+	public static void main(String[] args) {
+
+		Client client = new Client("127.0.0.1");
+		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		client.startRunning();
+
+	}
+
+}
